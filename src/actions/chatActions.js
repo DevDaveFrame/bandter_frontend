b1561d0a818e8381fd652e894e83b87d44c1871d
DevDaveFrame <dframe22@gmail.com>
2020-12-08 12:43:31 -0500

    moved handleMatch to chatActions

--- src/actions/chatActions.js
+++ src/actions/chatActions.js
@@ -2,6 +2,23 @@ export function setAsCurrentChat (chat) {
   return { type: "SET_CURRENT_CHAT", current: chat.id }
 }
 
+export function handleMatch (friendObj) {
+  return (dispatch) => {
+    dispatch({type: "REMOVE_FRIENDEE", friendee_id: friendObj.friendee_id});
+    let request = {
+      method: "POST",
+      headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${localStorage.token}`
+      },
+      body: JSON.stringify(friendObj)
+    }
+    fetch(`http://localhost:3000/api/v1/friend_request`, request)
+    .then(r => r.json())
+    .then(match => dispatch({type: 'ADD_MATCH', match}) )
+  }
+};
+
 export function pushToCurrentChat (message) {
   console.log('message: ', message);
   return { type: "ADD_MESSAGE", message: message.data.attributes }
--- src/actions/discoverActions.js
+++ src/actions/discoverActions.js
@@ -15,26 +15,3 @@ export function beginSearch (formObj) {
     .catch(console.log)
   } 
 };
-
-export function handleMatch (friendObj) {
-  return (dispatch) => {
-    dispatch({type: "REMOVE_FRIENDEE", friendee_id: friendObj.friendee_id});
-    let request = {
-      method: "POST",
-      headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${localStorage.token}`
-      },
-      body: JSON.stringify(friendObj)
-    }
-    fetch(`http://localhost:3000/api/v1/friend_request`, request)
-    .then(r => r.json())
-    .then(match => dispatch({type: 'ADD_MATCH', match}) )
-  }
-};
-
-export function logoutUser() {
-  return {
-    type: 'LOGOUT_USER',
-  }
-};
